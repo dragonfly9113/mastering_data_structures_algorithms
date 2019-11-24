6f077dd1b1bec53f255dd1ebf611dcfc9fc74bf4
William Luo <weilin.william.luo@gmail.com>
2019-11-23 21:37:59 -0800

    Initial commit.

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,62 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+struct Rectangle {
+	int length;
+	int breadth;
+};
+
+
+struct Point {
+	int x;
+	int y;
+};
+
+struct Complex {
+	int real;
+	int img;
+};
+
+struct Student {
+	int roll;
+	char name[25];
+	char dep[10];
+	char address[50];
+};
+
+
+int main(int argc, char* argv[]) {
+
+	int A[5];
+
+	int B[] = { 2, 4, 6, 8, 10 };
+
+	char name[25] = "Mike";
+
+	for (int i = 0; i < 5; i++) {
+
+		printf("B[%d] = %d\n", i, B[i]);
+
+	}
+
+	struct Rectangle r = { 10, 5 };
+	struct Point p;
+	struct Student s;
+	s.roll = 10;
+	//s.name = "John";
+	strcpy_s(s.name, "John");
+	//s.address = "1234 Albert Pl.";
+
+	printf("%d %d\n", r.length, r.breadth);
+	printf("Area of Rectangle is %d\n", r.length * r.breadth);
+
+	printf("name = %s\n", name);
+
+	return 0;
+}
